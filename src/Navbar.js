4c182d1db39e3e7639d44309155dda322c3006c5
Tony Leng <tleng2@illinois.edu>
2024-07-14 01:46:01 -0500

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>Tony's Blog</h1>
+            <h1>(your name here @navbar.js)'s Blog</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
